653a6e91d1a8e3cde68fa9c2b54d9e5e8eb2e314
Michael Plekunov <mplekunov@gmail.com>
2022-05-25 00:50:00 -0400

    HotFix: Version 2.1
    Functions are highlighted in Editing Mode

--- app/src/main/java/com/example/calculator/model/input/expandedEditing/ExpandedClickableFunction.kt
+++ app/src/main/java/com/example/calculator/model/input/expandedEditing/ExpandedClickableFunction.kt
@@ -8,6 +8,7 @@ import android.text.style.DynamicDrawableSpan
 import android.text.style.ImageSpan
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
 import com.example.calculator.R
@@ -30,27 +31,23 @@ class ExpandedClickableFunction(
     viewModel: CalculatorViewModel,
     liveInput: MutableLiveData<SpannableStringBuilder>,
     index: Int
-) : Clickable(context, buttons, viewModel, liveInput, index) {
+) : ExpandedClickable(context, buttons, viewModel, liveInput, index) {
 
     override lateinit var oldString: String
 
     override val what
         get() = ExpandedClickableFunction(context, buttons, viewModel, liveInput, index)
 
-    override fun onClick(view: View) {
-        super.onClick(view)
-    }
-
     override fun bindToEditableToken() {
         buttons.operators.forEach { (button, _) -> setButtonState(button, disabledButtonColor, false) }
         buttons.numbers.forEach { (button, _) -> setButtonState(button, disabledButtonColor, false) }
 
+        setButtonState(buttons.clear, disabledButtonColor, false)
+        setButtonState(buttons.clearAll, disabledButtonColor, false)
+
         setButtonState(buttons.functions[FunctionKind.PERCENTAGE], disabledButtonColor, false)
         setButtonState(buttons.functions[FunctionKind.SQUARED], disabledButtonColor, false)
         setButtonState(buttons.functions[FunctionKind.FACTORIAL], disabledButtonColor, false)
-
-        setButtonState(buttons.clear, disabledButtonColor, false)
-        setButtonState(buttons.clearAll, disabledButtonColor, false)
 
         buttons.functions.forEach { (button, function) ->
             button.setOnClickListener {
@@ -70,42 +67,10 @@ class ExpandedClickableFunction(
 
                 if (viewModel.set(function, index)) {
                     replaceSpan(viewModel.formattedInput[index])
+                    setDrawableSpan()
                     applyColorToSpan(highlightedColor, newStart, newEnd)
                 }
             }
         }
-    }
-
-
-    override fun replaceSpan(newString: String) {
-        setDrawableSpan(newString)
-    }
-
-    private fun setDrawableSpan(newString: String) {
-        spannable.replace(oldStart, oldEnd, newString)
-
-        val token = viewModel.inputAsTokens[index]
-
-        val drawable = when(token.type) {
-            TokenTypes.Function -> buttons.functions[FunctionParser.parse<FunctionKind>(token)]!!.drawable.constantState!!.newDrawable().mutate()
-            TokenTypes.Number -> buttons.numbers[NumberParser.parse<NumberKind>(token)]!!.drawable.constantState?.newDrawable()!!.mutate()
-            TokenTypes.Operator -> buttons.operators[OperatorParser.parse<OperatorKind>(token)]!!.drawable.constantState!!.newDrawable().mutate()
-        }
-
-        drawable.setTint(ContextCompat.getColor(context, R.color.white))
-        val size: Int = (context as Activity).findViewById<TextView>(R.id.input).textSize.toInt()
-        drawable.setBounds(0, 0,  size - 15, size - 20)
-
-        if (FunctionParser.parse<FunctionKind>(token) == FunctionKind.LOG)
-            drawable.setBounds(0, 0, size + 45, size)
-
-        spannable.setSpan(
-            ImageSpan(
-                drawable,
-                DynamicDrawableSpan.ALIGN_CENTER
-            ), newStart, newEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        spannable.setSpan(newStart, newEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
     }
 }
